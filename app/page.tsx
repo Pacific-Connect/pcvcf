808f9a6cc8b6067a0a169628ec5e672c1a2014d3
MOKOJOPE <momoinbb21@gmail.com>
2025-03-28 17:38:54 -1000

    Update text content in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.123
+            Save and see your changes instantly.123123chgjcgh
+            vguvjhkvhcgchvchchhhvghv ghvh
           </li>
         </ol>
 
