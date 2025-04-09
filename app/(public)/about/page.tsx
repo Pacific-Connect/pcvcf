908f91d1acc584dec9fe1e416777ebca786610ce
DanielH987 <hootinid@go.byuh.edu>
2025-04-09 13:05:14 -1000

    Update AboutPage to include Daniel's avatar with social media links and adjust layout

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -4,9 +4,11 @@ export default function AboutPage() {
     return (
         <div>
             <h1>About Us</h1>
-            <Avatar src="/favicon.ico" alt="avatar img" name="Daniel" title='Developer'/>
-            <Avatar src="/favicon.ico" alt="avatar img" name="Momo" title='Developer'/>
-            <Avatar src="/favicon.ico" alt="avatar img" name="Jesse" title='PM'/>
+            <div style={{ padding: '10px' }}>
+                <Avatar src="/daniel.jpeg" alt="avatar img" name="Daniel" title='Developer' linkedinUrl='https://www.linkedin.com/in/daniel-hootini/' githubUrl='https://github.com/DanielH987' porfotlioUrl='https://danielh987.github.io/portfolio/'/>
+                <Avatar src="/favicon.ico" alt="avatar img" name="Momo" title='Developer'/>
+                <Avatar src="/favicon.ico" alt="avatar img" name="Jesse" title='PM'/>
+            </div>
             <p>Welcome to our website! We are passionate about building amazing experiences for our users.</p>
         </div>
     );
