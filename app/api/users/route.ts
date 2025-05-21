108b0995042906c574695e0b42ecd4714ba1632c
Om4njuu <omanjuu.20@gmail.com>
2025-05-20 21:24:17 -1000

    Implement password visibility toggle in Login and Register pages; add delete user api function

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,3 +5,15 @@ export async function GET() {
   const users = await db.user.findMany();
   return NextResponse.json(users);
 }
+
+export async function DELETE(request: Request) {
+  try {
+    const { id } = await request.json();
+    const user = await db.user.delete({
+      where: { id },
+    });
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+}
